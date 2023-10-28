9d0d2dad04a8354f926cb03a465df06e878121f9
Danial <danialdark12098@gmail.com>
2023-10-28 11:03:56 +0330

    changing it

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 const soap = require('soap');
 const crypto = require('crypto');
 
-async function test() {
-    const url = 'http://94.232.171.25:11003/PublicService.svc';
+// URL of the SOAP web service WSDL
+const url = 'http://94.232.171.25:11003/PublicService.svc?wsdl';
 
-    const client = await soap.createClientAsync(url, {
-        binding: 'http://schemas.xmlsoap.org/wsdl/soap/http',
-        security: {
-            mode: 'WS',
-            username: 'chartix',
-            password: hashToSHA1('P(&e6]8Dyk}tr>)'),
-        },
-        wsdl_options: {
-            // Set any additional options here.
-        },
+// Create a SOAP client
+soap.createClient(url, (err, client) => {
+    if (err) {
+        console.error('Error creating SOAP client:', err);
+        return;
+    }
+
+    // Set the SOAP request headers (Username and Password)
+    client.setSecurity(new soap.BasicAuthSecurity('test', sha1Hash('123')));
+
+    // Make a SOAP request to the "GetSpotOffers" method
+    client.GetSpotOffers({}, (err, result) => {
+        if (err) {
+            console.error('SOAP request error:', err);
+            return;
+        }
+
+        // Process the SOAP response
+        console.log('SOAP Response:', result);
     });
+});
 
-    const result = await client.GetSpotOffersAsync();
-
-    console.log(result);
-}
-
-function hashToSHA1(input) {
+// Function to hash a string using SHA-1
+function sha1Hash(input) {
     const sha1 = crypto.createHash('sha1');
     sha1.update(input, 'utf8');
     return sha1.digest('hex');
 }
-
-test();
